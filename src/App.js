f7c549f605f0b20511a292229863ec190f551ef2
Dancing <s990346@gmail.com>
2024-11-01 21:23:02 +0800

    testing

--- src/App.js
+++ src/App.js
@@ -15,61 +15,10 @@ import { Elements } from "@stripe/react-stripe-js"
 
 const promise = loadStripe(process.env.STRIPE_PUBLIC_KEY)
                           
-
 function App() {
-  const [{}, dispatch] = useStateValue();
-
-  useEffect(()=>{ //like if statement
-    // will only run once when the app component loads
-    auth.onAuthStateChanged(authUser=>{
-      console.log('The User is >>>', authUser)
-      if (authUser){
-        // the user just logged in 
-        dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
-      } else {
-        // the user is logged out
-        dispatch({
-        type: 'SET_USER', 
-        user: null
-        })
-      }
-    })
-  }, [])
-
-
   return (
-    <Router>
-      <div className="app">
-        <Routes>
-        <Route path="/orders" element={<React.Fragment> 
-            <Header />
-            <Orders />
-          </React.Fragment>} />
-          <Route path="/login" element={<React.Fragment> 
-            <Login />
-          </React.Fragment>} />
-          <Route path="/checkout" element={<React.Fragment> 
-            <Header />
-            <Checkout />
-          </React.Fragment>} />
-          <Route path="/payment" element={<React.Fragment> 
-            <Header />
-            <Elements stripe={promise}>
-              <Payment />
-            </Elements>
-          </React.Fragment>} />
-          <Route path="/" element={<React.Fragment> 
-            <Header />
-            <Home />
-            <Footer />
-          </React.Fragment>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+      <Footer />
+    </div>
   );
 }
-
-export default App;
