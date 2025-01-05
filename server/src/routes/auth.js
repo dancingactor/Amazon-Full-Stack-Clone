c25aa71be8e378ed04cb51e7fc3dfb46a4b9c798
Dancing <s990346@gmail.com>
2025-01-05 20:44:34 +0800

    server auth api completed

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -7,7 +7,7 @@ router.post("/register", async (req, res) => {
     try {
         const { email, password } = req.body;
         
-        // Check if user already exists, if user exists, return the user info. Otherwise, return none
+        // Check if user already exists in DB, if user exists, return the user info. Otherwise, return none
         const existingUser = await prisma.user.findUnique({ where: { email } });
         if (existingUser) {
             return res.status(400).json({ msg: "User already exists" });
@@ -15,7 +15,7 @@ router.post("/register", async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Create a User
+        // Create a User in DB
         const newUser = await prisma.user.create({
             data: {
                 email,
@@ -38,3 +38,36 @@ router.post("/register", async (req, res) => {
         res.status(500).json({ error: "Server error"} )
     }
 })
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+        
+        // if user exists, check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ error: "Invalid credentials" });
+        }
+
+        // Generate JWT token
+        const token = await jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+
+        res.json({ 
+            message: 'Logged in successfully', 
+            token, 
+            user: { id: user.id, email: user.email } 
+          });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error"});
+    };
+});
+
+module.exports = router;
