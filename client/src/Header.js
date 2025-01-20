1fecf28928daf23895510396abeae7eccc63692b
Dancing <s990346@gmail.com>
2025-01-20 10:48:50 +0800

    Update the shopping cart logo

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './Header.css';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import { Link } from 'react-router-dom';
-import { useStateValue } from './StateProvider';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
 function Header() {
@@ -13,7 +13,7 @@ function Header() {
     if (user) {
       auth.signOut();
     } else {
-      // Handle Sign In      
+      // Handle Sign In
     }
   };
 
@@ -33,13 +33,13 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to={!user && '/login'}>
+        <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header__option">
             <span className="header__optionLineOne">
-              Hello, {!user ? 'Guest' : user.email}
+              Hello, {!user ? "Guest" : user.email}
             </span>
             <span className="header__optionLineTwo">
-              {user ? 'Sign Out' : 'Sign In'}
+              {user ? "Sign Out" : "Sign In"}
             </span>
           </div>
         </Link>
@@ -52,13 +52,14 @@ function Header() {
         </Link>
 
         <div className="header__option">
-          <span className="header__optionLineOne">Your</span> {/* Fixed extra space */}
+          <span className="header__optionLineOne">Your</span>{" "}
+          {/* Fixed extra space */}
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
         <Link to="/checkout">
           <div className="header__optionBasket">
-            <ShoppingBasketIcon />
+            <ShoppingCartOutlinedIcon />
             <span className="header__optionLineTwo header__basketCount">
               {basket?.length}
             </span>
