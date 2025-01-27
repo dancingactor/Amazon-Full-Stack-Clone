c0d95dde442647a80471fc712041464d8fe261d9
Dancing <s990346@gmail.com>
2025-01-27 13:56:58 +0800

    complete the getBasket function

--- server/src/routes/basket.js
+++ server/src/routes/basket.js
@@ -1,8 +1,38 @@
 const router = require('express').Router();
 const { PrismaClient } = require("@prisma/client");
+const authenticateToken = require('./middleware/authenticateToken');
 
-const prisma = PrismaClient();
+const prisma = new PrismaClient();
 
-router.post("/", (res, req) => {
-    
-})
+// get someone's basket
+router.get("/", authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        let basket = await prisma.basket.findUnique({ 
+            where: { userId },
+            include: {
+                items: {
+                    include: { product: true }
+                }
+            }
+        });
+        
+        if (!basket) {
+            basket = await prisma.basket.create({
+                data: { userId },
+                include: {
+                    items: {
+                        include: {
+                            product:true
+                        }
+                    }
+                }
+            });
+        }
+
+        res.json(basket);
+    } catch (error) {
+        console.error("Error fetching basket", error);
+        res.status(500).json(error: "Server error");
+    }
+});
