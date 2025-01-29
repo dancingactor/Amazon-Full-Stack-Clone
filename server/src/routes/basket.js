141f0cdafb60625b9ba7b73117a833d730709f43
Dancing <s990346@gmail.com>
2025-01-29 10:30:20 +0800

    complete the backend logic of add an element to basket

--- server/src/routes/basket.js
+++ server/src/routes/basket.js
@@ -43,5 +43,34 @@ router.post("/", authenticateToken, async (req, res) => {
         const userId = req.user.id;
         const { productId, quantity } = req.body;
         
+        const product = await prisma.product.findUnique({ where: { id: productId }});
+        if (!product) {
+            return res.status(404).json({ error: "Product not found"});
+        }
+
+        const basket = await prisma.basket.findUnique({ where: { userId } });
+        if (!basket) {
+            await prisma.basket.create({ data: { userId } });
+        }
+
+        let basketItem = await prisma.basketItem.findUnique({
+            where: { baksetId: basket.id, productId }
+        })
+
+        if (basketItem) {
+            basketItem = await prisma.basketItem.update({
+                where: { id: basketItem.id },
+                data: { quantity: basketItem.quantity + quantity }
+            });
+        } else {
+            basketItem = await prisma.basketItem.create({
+                data: { basketId: basket.id, productId, quantity }
+            })
+        }
+
+        res.json(basketItem);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error"});
     }
-})
+});
