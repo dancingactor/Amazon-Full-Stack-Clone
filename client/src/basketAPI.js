c4f59e078f55f59bbdcff2e548c76d8e330e7208
Dancing <s990346@gmail.com>
2025-02-02 17:59:44 +0800

    Update the backend URL of basketAPI

--- client/src/basketAPI.js
+++ client/src/basketAPI.js
@@ -1,4 +1,6 @@
-const backendUrl = "http://localhost:5000";
+require('dotenv').config();  
+
+const backendUrl = "http://localhost:5000" || process.env.REACT_APP_BACKEND_URL;
 
 export const fetchBasket = async (token) => {
     try {
