71f787cb8470cfd776696b8fbc83836e128ce1ec
Dancing <s990346@gmail.com>
2025-02-02 18:14:23 +0800

    Update the backend to listen at 8080 port

--- server/src/index.js
+++ server/src/index.js
@@ -21,7 +21,7 @@ app.get('*', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
